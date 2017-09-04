d804fb6ed349e459134cd32a3af8c959a8459e14
Jon Haddad <jon@jonhaddad.com>
2017-09-04 12:17:25 -0700

    testing the server

---
+++ src/main/kotlin/GossipListenerService.kt
@@ -0,0 +1,5 @@
+import com.rustyrazorblade.gossiptop.GossipServiceGrpc.GossipServiceImplBase
+
+class GossipListenerService : GossipServiceImplBase() {
+
+}
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,12 @@
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+
 fun main(args: Array<String>) {
     println("Running the server!")
+
+    var server = ServerBuilder.forPort(5000).addService(GossipListenerService()).build()
+    server.awaitTermination()
+
 }
+
+
